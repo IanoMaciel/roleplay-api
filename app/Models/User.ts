a095560e2eb721d5fc1bb84254bc3e5c42504a4d
Iano Maciel <ianomaciel6385@gmail.com>
2023-11-05 14:31:53 -0400

    feat: criptografando dados sensíveis do usuário

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,25 +1,32 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, beforeSave, column } from '@ioc:Adonis/Lucid/Orm'
+import Hash from '@ioc:Adonis/Core/Hash'
 
 export default class User extends BaseModel {
-    @column({ isPrimary: true })
-    public id: number
+  @column({ isPrimary: true })
+  public id: number
 
-    @column()
-    username: string
+  @column()
+  username: string
 
-    @column()
-    email: string
+  @column()
+  email: string
 
-    @column()
-    password: string
+  @column({ serializeAs: null })
+  password: string
 
-    @column()
-    avatar: string
+  @column()
+  avatar: string
 
-    @column.dateTime({ autoCreate: true })
-    public createdAt: DateTime
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
 
-    @column.dateTime({ autoCreate: true, autoUpdate: true })
-    public updatedAt: DateTime
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password)
+      user.password = await Hash.make(user.password)
+  }
 }
