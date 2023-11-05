8dc555029bd3d67617d4fb1258c4e31ed15e3d87
Iano Maciel <ianomaciel6385@gmail.com>
2023-11-05 18:56:36 -0400

    feat: testando quando email e username estão em uso

--- test/users/user.spec.ts
+++ test/users/user.spec.ts
@@ -1,14 +1,14 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import { UserFactory } from 'Database/factories'
 import test from 'japa'
 import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
-test.group('User', () => {
+test.group('User', (group) => {
   test('it should create an user', async (assert) => {
     const userPayload = { email: 'test@test.com', username: 'test', password: 'test123', avatar: 'http://test.com' }
     const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
-
-    console.log({ body })
 
     assert.exists(body.user, 'User undefined')
     assert.exists(body.user.id, 'Id undefined')
@@ -17,4 +17,47 @@ test.group('User', () => {
     assert.equal(body.user.avatar, userPayload.avatar)
     assert.notExists(body.user.password, 'Password defined')
   })
+
+  test('it should return 409 when email is already in use', async (assert) => {
+    const { email } = await UserFactory.create()
+    const { body } = await supertest(BASE_URL).post('/users').send({
+      email,
+      username: 'test',
+      password: 'test',
+      avatar: 'http://test.com'
+    }).expect(409)
+
+    assert.exists(body.message)
+    assert.exists(body.code)
+    assert.exists(body.status)
+    assert.include(body.message, 'email')
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 409)
+  })
+
+  test('it should return 409 when username is already in use', async (assert) => {
+    const { username } = await UserFactory.create()
+    const { body } = await supertest(BASE_URL).post('/users').send({
+      username,
+      email: 'test@test.com',
+      password: 'test',
+      avatar: 'http://test.com'
+    }).expect(409)
+
+    assert.exists(body.message)
+    assert.exists(body.code)
+    assert.exists(body.status)
+    assert.include(body.message, 'username')
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 409)
+  })
+
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
+
 })
