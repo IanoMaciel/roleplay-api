df4b02f7180f566f7c0cbfbd3597b3b0c5627c52
Iano Maciel <ianomaciel6385@gmail.com>
2023-11-05 14:33:42 -0400

    chore: configurações

--- test/users/user.spec.ts
+++ test/users/user.spec.ts
@@ -4,13 +4,17 @@ import supertest from 'supertest'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('User', () => {
-    test('it should create an user', async (assert) => {
-        const userPayload = {
-            email: 'test@test.com',
-            username: 'test',
-            password: 'test123',
-            avatarUrl: 'http://test.com'
-        }
-        await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
-    })
+  test('it should create an user', async (assert) => {
+    const userPayload = { email: 'test@test.com', username: 'test', password: 'test123', avatar: 'http://test.com' }
+    const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+
+    console.log({ body })
+
+    assert.exists(body.user, 'User undefined')
+    assert.exists(body.user.id, 'Id undefined')
+    assert.equal(body.user.email, userPayload.email)
+    assert.equal(body.user.username, userPayload.username)
+    assert.equal(body.user.avatar, userPayload.avatar)
+    assert.notExists(body.user.password, 'Password defined')
+  })
 })
