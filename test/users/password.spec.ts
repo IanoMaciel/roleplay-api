b831dee135ce6d78b2530c569e08fb12eb310b7f
Iano Maciel <ianomaciel6385@gmail.com>
2023-11-11 20:22:29 -0400

    test: criação da estrutura do teste da senha

---
+++ test/users/password.spec.ts
@@ -0,0 +1,30 @@
+import Mail from '@ioc:Adonis/Addons/Mail'
+import Database from '@ioc:Adonis/Lucid/Database'
+import { UserFactory } from 'Database/factories'
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+
+test.group('Password', (group) => {
+  test.only('it should send and email with forgot password instructions', async (assert) => {
+    const user = await UserFactory.create()
+    Mail.trap((message) => {
+      assert.equal(message.subject, 'Roleplay: Recuperação de senha')
+    })
+    await supertest(BASE_URL).post('/forgot-password').send({
+      email: user.email,
+      resetPasswordUrl:  'url'
+    }).expect(204)
+
+    Mail.restore()
+  })
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
+})
