a6c4d38a635ef72e59d0cd8bc36a3cdaf7291afd
Iano Maciel <ianomaciel6385@gmail.com>
2023-11-11 20:23:17 -0400

    chore: configuração do mail

--- start/routes.ts
+++ start/routes.ts
@@ -24,4 +24,7 @@ Route.get('/', async () => {
     return { hello: 'world' }
 })
 Route.post('/users', 'UsersController.store')
-Route.put('users/:id', 'UsersController.update')
+Route.put('/users/:id', 'UsersController.update')
+
+Route.post('/forgot-password', 'PasswordsController.forgotPassword')
+
