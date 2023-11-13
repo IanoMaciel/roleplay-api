0753490f9333cf61e0317d547692b598faa3016b
Iano Maciel <ianomaciel6385@gmail.com>
2023-11-13 19:09:42 -0400

    test: utilizando template html para envio de emails

--- app/Controllers/Http/PasswordsController.ts
+++ app/Controllers/Http/PasswordsController.ts
@@ -1,16 +1,22 @@
 import Mail from '@ioc:Adonis/Addons/Mail'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { UserFactory } from 'Database/factories'
+import User from 'App/Models/User'
 
 export default class PasswordsController {
   public async forgotPassword({ request, response }: HttpContextContract) {
-    const { email } = request.only(['email'])
+    const { email, resetPasswordUrl  } = request.only(['email', 'resetPasswordUrl'])
+    const user = await User.findByOrFail('email', email)
+
     await Mail.send((message) => {
       message
         .from('no-replay@roleplay.com')
         .to(email)
         .subject('Roleplay: Recuperação de senha')
-        .text('Clique no link abaixo para redefinição de senha')
+        .htmlView('email/forgotpassword', {
+          productName: 'Roleplay',
+          name: user.username,
+          resetPasswordUrl,
+        })
     })
     return response.noContent()
 
--- test/users/password.spec.ts
+++ test/users/password.spec.ts
@@ -1,4 +1,4 @@
-import Mail from '@ioc:Adonis/Addons/Mail'
+//import Mail from '@ioc:Adonis/Addons/Mail'
 import Database from '@ioc:Adonis/Lucid/Database'
 import { UserFactory } from 'Database/factories'
 import test from 'japa'
@@ -10,19 +10,22 @@ const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 test.group('Password', (group) => {
   test.only('it should send and email with forgot password instructions', async (assert) => {
     const user = await UserFactory.create()
-    Mail.trap((message) => {
-      assert.deepEqual(message.to, [{ address: user.email }])
-      assert.deepEqual(message.from, { address: 'no-replay@roleplay.com' })
-      assert.equal(message.subject, 'Roleplay: Recuperação de senha')
-      assert.equal(message.text, 'Clique no link abaixo para redefinição de senha')
-    })
+
+    // Mail.trap((message) => {
+    //   assert.deepEqual(message.to, [{ address: user.email }])
+    //   assert.deepEqual(message.from, { address: 'no-replay@roleplay.com' })
+    //   assert.equal(message.subject, 'Roleplay: Recuperação de senha')
+    //   assert.include(message.html!, user.username)
+    // })
+
     await supertest(BASE_URL).post('/forgot-password').send({
       email: user.email,
       resetPasswordUrl:  'url'
     }).expect(204)
 
-    Mail.restore()
-  })
+    // Mail.restore()
+
+  }).timeout(5000)
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction()
   })
