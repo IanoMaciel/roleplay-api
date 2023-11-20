9f69d0d575fea3bd2afbb0111f438b0d50480f9b
Iano Maciel <ianomaciel6385@gmail.com>
2023-11-19 23:52:51 -0400

    test: adicionando o mestre como usuário da mesa

--- test/groups/groups.spec.ts
+++ test/groups/groups.spec.ts
@@ -25,10 +25,13 @@ test.group('Groups', (group) => {
     assert.equal(body.group.location, groupPayload.location)
     assert.equal(body.group.chronic, groupPayload.chronic)
     assert.equal(body.group.master, groupPayload.master)
+    assert.exists(body.group.players, 'Player undefined')
+    assert.equal(body.group.players.length, 1)
+    assert.equal(body.group.players[0].id, groupPayload.master)
 
   })
 
-  test.only('it should 422 when required data is not provided', async (assert) => {
+  test('it should 422 when required data is not provided', async (assert) => {
     const { body } = await supertest(BASE_URL).post('/groups').send({}).expect(422)
     assert.equal(body.code, 'BAD_REQUEST')
     assert.equal(body.status, 422)
