6cf06e55f1b17f25cbf955ccbd917eaef6db0996
Iano Maciel <ianomaciel6385@gmail.com>
2023-12-29 11:04:45 -0400

    test: cenário de erro no aceite de solicitação para mesas

--- test/groups/group_request.spec.ts
+++ test/groups/group_request.spec.ts
@@ -124,7 +124,7 @@ test.group('Group request', (group) => {
     assert.equal(body.status, 422)
   })
 
-  test.only('it should accept a group request', async (assert) => {
+  test('it should accept a group request', async (assert) => {
     const master = await UserFactory.create();
     const group = await GroupFactory.merge({ master: master.id }).create()
 
@@ -146,6 +146,40 @@ test.group('Group request', (group) => {
     assert.isNotEmpty(group.players)
     assert.equal(group.players.length, 1)
     assert.equal(group.players[0].id, user.id)
+  })
+
+  test('it should return 404 when providing an unexisting group', async (assert) => {
+    const master = await UserFactory.create();
+    const group = await GroupFactory.merge({ master: master.id }).create()
+
+    const { body } = await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({})
+
+    const response = await supertest(BASE_URL)
+      .post(`/groups/123/requests/${body.groupRequest.id}/accept`)
+      .expect(404)
+
+    assert.equal(response.body.code, 'BAD_REQUEST')
+    assert.equal(response.body.status, 404)
+  })
+
+  test('it should return 404 when providing an unexisting group request', async (assert) => {
+    const master = await UserFactory.create();
+    const group = await GroupFactory.merge({ master: master.id }).create()
+
+    await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({})
+
+    const response = await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests/123/accept`)
+      .expect(404)
+
+    assert.equal(response.body.code, 'BAD_REQUEST')
+    assert.equal(response.body.status, 404)
   })
 
   group.before(async () => {
