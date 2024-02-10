95362035a401336532ec198333bce67fe924df63
Iano Maciel <ianomaciel6385@gmail.com>
2024-02-10 09:31:10 -0400

    refactor: addition of cascade in table request migration

--- database/migrations/1700948201601_group_requests.ts
+++ database/migrations/1700948201601_group_requests.ts
@@ -3,17 +3,17 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class GroupRequests extends BaseSchema {
   protected tableName = 'groups_requests'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.integer('user_id').unsigned().references('id').inTable('users').notNullable()
-      table.integer('group_id').unsigned().references('id').inTable('groups').notNullable()
+      table.integer('group_id').unsigned().references('id').inTable('groups').onDelete('CASCADE').notNullable()
       table.enum('status', ['PENDING', 'ACCEPTED']).defaultTo('PENDING').notNullable()
       table.timestamps(true)
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
