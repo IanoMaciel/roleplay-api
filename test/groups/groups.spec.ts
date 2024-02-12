eb97224c2288ab980d575da6e4f68055ce5cb30f
Iano Maciel <ianomaciel6385@gmail.com>
2024-02-12 08:35:12 -0400

    test: it should return all groups by description

--- test/groups/groups.spec.ts
+++ test/groups/groups.spec.ts
@@ -410,6 +410,51 @@ test.group('Groups', (group) => {
     assert.equal(body.groups[0].players[0].username, user.username)
   })
 
+  test('it should return all groups by description', async (assert) => {
+    const groupPayload = {
+      name: '123',
+      description: 'test description',
+      schedule: 'every day',
+      location: 'test',
+      chronic: 'test lalalalallalala',
+      master: user.id
+    }
+
+    const response = await supertest(BASE_URL)
+      .post('/groups')
+      .set('Authorization', `Bearer ${token}`)
+      .send(groupPayload).expect(201)
+
+    await supertest(BASE_URL)
+      .post('/groups')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ ...groupPayload, name: '123', description: '1234' }).expect(201)
+
+    const group = response.body.group
+
+    const { body } = await supertest(BASE_URL)
+      .get(`/groups?&text=es`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+
+    assert.exists(body.groups, 'Groups undefined')
+    assert.equal(body.groups.length, 1)
+    assert.equal(body.groups[0].id, group.id)
+    assert.equal(body.groups[0].name, group.name)
+    assert.equal(body.groups[0].description, group.description)
+    assert.equal(body.groups[0].schedule, group.schedule)
+    assert.equal(body.groups[0].location, group.location)
+    assert.equal(body.groups[0].chronic, group.chronic)
+    assert.exists(body.groups[0].masterUser, 'Master undefined')
+    assert.equal(body.groups[0].masterUser.id, user.id)
+    assert.equal(body.groups[0].masterUser.username, user.username)
+    assert.isNotEmpty(body.groups[0].players, 'Empty players')
+    assert.equal(body.groups[0].players[0].id, user.id)
+    assert.equal(body.groups[0].players[0].email, user.email)
+    assert.equal(body.groups[0].players[0].username, user.username)
+  })
+
+
   group.before(async () => {
     const plainPassword = '1234567'
     const newUser = await UserFactory.merge({ password: plainPassword }).create()
