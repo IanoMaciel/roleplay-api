fa399b807978ccdb9b8795a4a913ecd2d058f668
Iano Maciel <ianomaciel6385@gmail.com>
2024-02-11 20:41:02 -0400

    test: it shoul return all groups when no query is provided to list groups

--- test/groups/groups.spec.ts
+++ test/groups/groups.spec.ts
@@ -177,6 +177,44 @@ test.group('Groups', (group) => {
     assert.equal(response.body.status, 404)
   })
 
+  test('it shoul return all groups when no query is provided to list groups', async (assert) => {
+    const groupPayload = {
+      name: 'test',
+      description: 'test description',
+      schedule: 'every day',
+      location: 'test',
+      chronic: 'test lalalalallalala',
+      master: user.id
+    }
+    const response = await supertest(BASE_URL)
+      .post('/groups')
+      .set('Authorization', `Bearer ${token}`)
+      .send(groupPayload).expect(201)
+
+    const group = response.body.group
+
+    const { body } = await supertest(BASE_URL)
+      .get('/groups')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+
+    assert.exists(body.groups, 'Groups undefined')
+    assert.equal(body.groups.length, 1)
+    assert.equal(body.groups[0].id, group.id)
+    assert.equal(body.groups[0].name, group.name)
+    assert.equal(body.groups[0].description, group.description)
+    assert.equal(body.groups[0].schedule, group.schedule)
+    assert.equal(body.groups[0].location, group.location)
+    assert.equal(body.groups[0].chronic, group.chronic)
+    assert.exists(body.groups[0].masterUser, 'Master undefined')
+    assert.equal(body.groups[0].masterUser.id, user.id)
+    assert.equal(body.groups[0].masterUser.username, user.username)
+    assert.isNotEmpty(body.groups[0].players, 'Empty players')
+    assert.equal(body.groups[0].players[0].id, user.id)
+    assert.equal(body.groups[0].players[0].email, user.email)
+    assert.equal(body.groups[0].players[0].username, user.username)
+  })
+
   group.before(async () => {
     const plainPassword = '1234567'
     const newUser = await UserFactory.merge({ password: plainPassword }).create()
@@ -203,3 +241,4 @@ test.group('Groups', (group) => {
     await Database.rollbackGlobalTransaction()
   })
 })
+
